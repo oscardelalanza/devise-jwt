b57f01e92c4db43779c51c8ea4cc1d06e40c3d69
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-11-16 08:25:52 -0800

    dispatch and revocation requests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,5 +312,12 @@ Devise.setup do |config|
   # ==> Configuration for jwt
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}],
+      ['POST', %r{^/register$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
   end
 end
